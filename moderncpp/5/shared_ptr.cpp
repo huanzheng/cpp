154340b2f7f39e47f8c8054709a5bc6b5e1d0ccc
huanzheng <huan.zheng.ray@gmail.com>
2020-09-11 14:14:08 +0800

    added 5
    Signed-off-by: huanzheng <huan.zheng.ray@gmail.com>

---
+++ moderncpp/5/shared_ptr.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <memory>
+
+void foo(std::shared_ptr<int> i)
+{
+    (*i)++;
+}
+int main()
+{
+    // auto pointer = new int(10); // illegal, no direct assignment
+    // Constructed a std::shared_ptr
+    auto pointer = std::make_shared<int>(10);
+    foo(pointer);
+    std::cout << *pointer << std::endl; // 11
+    // The shared_ptr will be destructed before leaving the scope
+    //
+
+    pointer = std::make_shared<int>(10);
+auto pointer2 = pointer; // reference count+1
+auto pointer3 = pointer; // reference count+1
+int *p = pointer.get(); // no increase of reference count
+std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl; // 3
+std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl; // 3
+std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl; // 3
+
+pointer2.reset();
+std::cout << "reset pointer2:" << std::endl;
+std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl; // 2
+std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl; // 0, pointer2 has reset
+std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl; // 2
+pointer3.reset();
+std::cout << "reset pointer3:" << std::endl;
+std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl; // 1
+std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl; // 0
+std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl; // 0, pointer3 has reset
+
+    return 0;
+}
